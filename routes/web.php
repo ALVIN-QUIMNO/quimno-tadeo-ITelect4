e6916ad5a31311f0183a1a80ac68eae5c27c95f5
zev07438@gmail.com <zev07438@gmail.com>
2025-05-30 11:08:30 +0800

    ash

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index'])->name('employee.index');
     Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create'])->name('employee.create');
     Route::post('employee/store', [App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.store');
+    Route::post('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit'])->name('employee.edit');
+    Route::post('employee/{id}/update', [App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.update');
+    Route::post('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete'])->name('employee.delete');
     //end of employee management
 
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
